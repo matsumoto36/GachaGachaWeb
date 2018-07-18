9fad030d69b2d21dbbfd35310a86ca5b5e2b64b1
DESKTOP-28F38VP\user <nkc.daiki.nishiwaki@gmail.com>
2018-07-18 15:18:22 +0900

    金が保持されるようになった

--- js/layout.js
+++ js/layout.js
@@ -1,15 +1,33 @@
-var kane = 5000;
+var kane = 0;
+var pointDataKey = "Point";
+window.onload = function(){
+    Get();
+    document.getElementById("kane").textContent = "残高　" + kane + "兆円";
+}
+
 function add(){
+    Get();
     kane = kane + 1000;
-    <!--alert(kane);-->
-        document.getElementById("kane").textContent = "残高　" + kane + "兆円";
+    document.getElementById("kane").textContent = "残高　" + kane + "兆円";
+    Set();
 }
 function sub(){
     if(kane >= 100){
+        Get();
         kane = kane - 100;
         document.getElementById("kane").textContent = "残高　" + kane + "兆円";
+        Set();
     }
     else{
         alert ("金が足りやがりません");
     }
 }
+function Get() {
+    var data = localStorage.getItem(pointDataKey);
+    kane = isNaN(data) ? 0 : Number(data);
+    <!--alert(kane);-->
+}
+function Set() {
+    localStorage.setItem(pointDataKey, kane);
+}
+
